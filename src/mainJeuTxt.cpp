d1c563667b872d969f4e8cba78239bc4b2ad97cb
Lucas <lucas.faustmann@gmail.com>
2022-03-31 14:48:30 +0200

    mainJeuTxt

--- src/mainJeuTxt.cpp
+++ src/mainJeuTxt.cpp
@@ -3,13 +3,8 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 int main ( int argc, char** argv ) 
 {
-=======
-
-int main () {
->>>>>>> 75ea0fd76a22aecee88656c3ce2daf8e138e693a
 	Jeu jeu;
 	txtBoucle(jeu);
 	return 0;
