a4b3b0be59b5539cb64b7c8acd97945515dd003a
Pierrick <pierrick.brossat@etu.univ-lyon1.fr>
2022-04-07 21:30:56 +0200

    lucas aides moi !

--- src/jeuMulti.cpp
+++ src/jeuMulti.cpp
@@ -65,6 +65,8 @@ unsigned int nbJoueurs=tabJoueur.size(); // nbJoueur= à la taille du tableau
         }
     nbPartie++;
 }
+//Attention au cas ou deux joueur ont le meme budget minimum !
+
 
 
 
@@ -74,7 +76,7 @@ void jeuMulti::initialisationMiseMulti()
 
     for (unsigned int i=0; i<tabJoueur.size(); i++) //parcours du tableau de joueurs
     {
-        tabJoueur[i].setGain(0);//initialisation des gais de tout les joueurs à 0
+        tabJoueur[i].setGain(0);//initialisation des gains de tout les joueurs à 0
     }
 
     vector<unsigned int>misePossible; //création d'un tableau avec les valeurs des mises possibles dedans
@@ -98,9 +100,6 @@ void jeuMulti::initialisationMiseMulti()
         // else Jeu.tabJoueur[i].initialisationMise(touche);               cas du joueur reel (Attention à comment rentrer la mise que l'on veut avec la version graphique)
 
     }
-
-
-
 
 }
 
@@ -134,6 +133,8 @@ void jeuMulti::initialisationJeuMulti()
 
 
 
+
+
 void jeuMulti::testRegression() const
 {
 
@@ -145,40 +146,56 @@ void jeuMulti::testRegression() const
     assert (unJeuMultiParDefaut.tabJoueur.size()==1); // taille =1
     cout<<"Test constructeur par défaut OK"<<endl;
     
-    jeuMulti unJeuMulti(3);
+
+
+    jeuMulti unJeuMulti(3); //3 signifie niveau=expert 
     assert (unJeuMulti.tabJoueur.size()==4); // taille =4
     assert (unJeuMulti.tabJoueur[0].nom=="moi"); //test sur le joueur reel
-    assert (unJeuMulti.tabJoueur[0].getNiveau() ==0);
+    assert (unJeuMulti.tabJoueur[0].getNiveau()==0);
     assert (unJeuMulti.tabJoueur[0].getBudget()==2000);
     assert (unJeuMulti.tabJoueur[1].nom=="IA1");
     assert (unJeuMulti.tabJoueur[2].nom=="IA2");
     assert (unJeuMulti.tabJoueur[3].nom=="IA3");
     for (int i=1; i<4; i++) //test sur les IA
     {
-    assert (unJeuMulti.tabJoueur[i].getNiveau() ==3);
+    assert (unJeuMulti.tabJoueur[i].getNiveau()==3);
     assert (unJeuMulti.tabJoueur[i].getBudget()==2000);
     }
     cout<<"Test du constructeur avec paramètre OK"<<endl;
+
 
 
     unJeuMulti.tabJoueur[3].setBudget(-2000); //le budget du joueur 4 (indice 3) est nul
     unJeuMulti.eliminationJoueur(); //elimination du du joueur 4 car il n'a plus d'argent
     assert (unJeuMulti.tabJoueur.size()==3); //verification qu'il est bien eliminé
 
-    // for (unsigned int i=0; i<unJeuMulti.tabJoueur.size(); i++)
-    // {
-    //     cout<<unJeuMulti.tabJoueur[i].nom<<endl;
-    // }
     unJeuMulti.tabJoueur[1].setBudget(-200); //budget du joueur 2 = 1800
     unJeuMulti.tabJoueur[2].setBudget(-300); //budget du joueur 2 = 1700
     unJeuMulti.nbPartie=6; //6 eme partie
     unJeuMulti.eliminationJoueur(); //il reste 3 joueur ET nous sommes a la partie n°6
     assert (unJeuMulti.tabJoueur.size()==2); //elimination du joueur 3 (indice 2)
 
-    for (unsigned int i=0; i<unJeuMulti.tabJoueur.size(); i++)
-    {
-        cout<<unJeuMulti.tabJoueur[i].nom<<endl;
-    }  
+    unJeuMulti.nbPartie=9; //9 eme partie
+    unJeuMulti.eliminationJoueur(); //il reste 2 joueur ET nous sommes a la partie n°9
+    assert (unJeuMulti.tabJoueur.size()==1); //elimination du joueur 2(indice 1) car il à 1800 contre 2000 pour le joueur 1 (indice 0)
+    cout<<"Test de eliminationJoueur() OK"<<endl;
 
+
+    jeuMulti unJeuMulti2(3); //création d'un nouveau jeuMulti (jeuMulti2)
+    cout<<"ok"<<endl;
+
+    unJeuMulti2.initialisationMiseMulti();
+    
+    for (unsigned int i=0; i<unJeuMulti2.tabJoueur.size(); i++)
+    {
+    assert (unJeuMulti2.tabJoueur[i].getGain()==0); //verification que gain = 0 pour tout les joueurs
+    }
+
+    for (unsigned int i=0; i<unJeuMulti2.tabJoueur.size(); i++)
+    {
+        assert ((unJeuMulti2.tabJoueur[i].getMise()==100)||(unJeuMulti2.tabJoueur[i].getMise()==200)||(unJeuMulti2.tabJoueur[i].getMise()==300)||(unJeuMulti2.tabJoueur[i].getMise()==500)||(unJeuMulti2.tabJoueur[i].getMise()==1000));
+        //verification que les mises jouées sont valides (100 ou 200 ou 300 ou 500 ou 1000)
+    }
+    cout<<"Test de initialisationMiseMulti() OK"<<endl;
 
 }
